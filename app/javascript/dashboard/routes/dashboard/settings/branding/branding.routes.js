b3dc50239342f12029e7343228e16ff2d935d41d
phmotad <phmotad@gmail.com>
2026-01-17 17:18:06 -0300

    fix: Corrige import de URLHelper em branding.routes.js para resolver build Docker

--- app/javascript/dashboard/routes/dashboard/settings/branding/branding.routes.js
+++ app/javascript/dashboard/routes/dashboard/settings/branding/branding.routes.js
@@ -1,4 +1,4 @@
-import { frontendURL } from '../../../helper/URLHelper';
+import { frontendURL } from '../../../../helper/URLHelper';
 import SettingsWrapper from '../SettingsWrapper.vue';
 
 export default {
